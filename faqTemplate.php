5795ce9d2447c29e35513b62d692e9a2711120c6
Sarah <sarahgriswold@hotmail.com>
2015-10-04 18:46:36 -0700

    website files
    initial files added for profile website

---
+++ faqTemplate.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Template Name: FAQ Page
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <title>FAQ Page</title>
+	</head>
+	<body>
+	<header class="banner">
+<h1>Frequently Asked Questions</h1>
+</header>
+<section>
+<h2>Question 1</h2>
+<p>This is the answer to question 1</p>
+
+<h2>Question 2</h2>
+<p>This is the answer to question 2</p>
+
+<h2>Question 3</h2>
+<p>This is the answer to question 3</p>
+</section>
+</body>
+</html>
